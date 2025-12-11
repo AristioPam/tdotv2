670e2b9ae2afd4007652c1836e1a965ccceb4fea
luciss41 <aristiov@gmail.com>
2025-12-11 20:06:29 +0700

    Update CanvasWrapper.tsx

--- src/components/CanvasWrapper.tsx
+++ src/components/CanvasWrapper.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import { Canvas } from '@react-three/fiber';
-import { Suspense } from 'react';
-import { Environment } from '@react-three/drei';
-import HeroScene from './HeroScene';
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Environment } from "@react-three/drei";
+import HeroScene from "./HeroScene";
 
 export default function CanvasWrapper() {
-    return (
-        <div className="absolute inset-0 -z-10 h-full w-full pointer-events-none">
-            <Canvas shadows dpr={[1, 2]}>
-                <Suspense fallback={null}>
-                    <HeroScene />
-                    <Environment preset="city" />
-                </Suspense>
-            </Canvas>
-        </div>
-    );
+  return (
+    <div className="absolute inset-0 -z-10 h-full w-full pointer-events-none">
+      <Canvas shadows dpr={[1, 1.4]}>
+        <Suspense fallback={null}>
+          <HeroScene />
+          <Environment preset="city" />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
